3f94efa48e15ed9dad703498727bba02b9d4eba6
atharva hiwase <atharva.hiwase07@gmail.com>
2021-02-11 11:58:26 +0530

    model and tables are created

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+# after creating  every model use command 'makemigrations', which will create a database schema 
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
