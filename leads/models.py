8ae303b58fe96a6e13418586c9f6f1073f20524e
atharva hiwase <atharva.hiwase07@gmail.com>
2021-02-21 13:11:53 +0530

    changes made

--- leads/models.py
+++ leads/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 # after creating  every model use command 'makemigrations', which will create a database schema 
@@ -7,3 +8,4 @@ class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+
